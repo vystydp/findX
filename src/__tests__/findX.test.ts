35dd69111ac19bdfde0f6669c90965bd4e450ba7
Petr <vystydp@gmail.com>
2024-07-02 11:29:27 +0200

    binary search

--- src/__tests__/findX.test.ts
+++ src/__tests__/findX.test.ts
@@ -1,9 +1,15 @@
-// src/__tests__/calculator.test.ts
 import { findXInArray } from '../findX';
 
-test('find x in an Array', () => {
-  
+test('find x in an array', () => {
   //ARRANGE
-  let arrayIncludingX = [1,2,3,4,555555,6,7,99999999,10]
-  expect(findXInArray(5, arrayIncludingX)).toBe(4);
+  let arrayIncludingX = [-1,1,2,2,3,4,5,5,5,6,7,7,7,7,9,10,11,23,45,999,999]
+  expect(findXInArray(999, arrayIncludingX)).toBe(arrayIncludingX.length - 2);
+});
+
+test('return -1 when x is missing in an array', () => {
+  //ARRANGE
+  let arrayIncludingX = [-1,1,2,2,3,4,5,5,5,6,7,7,7,7,9,10,11,23,45,999,999]
+  expect(findXInArray(-20, arrayIncludingX)).toBe(-1);
+  expect(findXInArray(999999, arrayIncludingX)).toBe(-1);
+  expect(findXInArray(8, arrayIncludingX)).toBe(-1);
 });
--- src/findX.ts
+++ src/findX.ts
@@ -1,14 +1,12 @@
-/**
-    You are given an array A of n integers. The elements of A are sorted in ascending order,
-    and are not necessarily unique. You are also given a target integer x. Implement a
-    search algorithm which finds a location j in the array such that all elements in the range
-    A[0], ..., A[j - 1] are strictly less than x, and all elements in the range A[j], ..., A[n - 1]
-    are greater than or equal to x. (Note that x itself need not appear in A for these
-    conditions to be satisfied.) If no suitable location is found, return -1. Your solution should
-    have runtime O(log n).
- */
-
-
 export function findXInArray(x: number, arr: Array<number>): number {
-    return 0;
+    let left: number = 0;
+    let right: number = arr.length;
+  
+    while (left < right) {
+      const mid: number = Math.floor((left + right) / 2);
+      if (x > arr[mid]) left = mid + 1;
+      else right = mid;
+    }
+  
+    return arr[left] === x ? left : -1;
 }
